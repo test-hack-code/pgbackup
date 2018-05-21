12d76ca3909ffad78561e7727cd79e121b1e1d0f
Vinay Nandipur Rudraiah <test.hack20161015@gmail.com>
2018-05-21 16:58:44 +0530

    Implemented CLI Parser

---
+++ src/pgbackup/cli.py
@@ -0,0 +1,27 @@
+
+import argparse
+
+known_drivers = ['local', 's3']
+
+class DriverAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unknown driver. Available drivers are 'local' & 's3'")
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="""
+    Back Up PostgreSQL databases locally or to AWS S3.
+    """)
+    parser.add_argument('url', help='URL of database to backkup')
+    parser.add_argument('--driver',
+            help='how & where to store backup',
+            nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
+            action=DriverAction,
+            required=True)
+    return parser
+
+
